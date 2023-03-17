edd0702c8c2b4cd5c624de964d9a88568899fb3f
KARTIKBANIK <kartikbanikshishir@gmail.com>
2023-03-18 00:33:28 +0600

    Update 1

--- twitter_clone/lib/contains/ui_constants.dart
+++ twitter_clone/lib/contains/ui_constants.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:twitter_clone/contains/constaints.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../theme/pallate.dart';
+
 class UIConstaints {
   static AppBar appBar() {
     return AppBar(
       title: SvgPicture.asset(
         AssetsConstants.twitterLogo,
+        color: Pallete.blueColor,
+        height: 30,
       ),
+      centerTitle: true,
     );
   }
 }
--- twitter_clone/lib/features/auth/view/login_view.dart
+++ twitter_clone/lib/features/auth/view/login_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/common/rounded_small_button.dart';
 import 'package:twitter_clone/contains/ui_constants.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_feild.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -9,10 +12,58 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final appbar = UIConstaints.appBar();
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: UIConstaints.appBar(),
+      appBar: appbar,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              children: [
+                //TextFeild 1..........
+                AuthFeild(
+                  controller: emailController,
+                  hintText: "Email",
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                //TextFeild 2..........
+                AuthFeild(
+                  controller: passController,
+                  hintText: "Password",
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                //Buttooon.........
+                RoundedSmallButton(
+                  onTap: () {},
+                  label: "Done",
+                ),
+
+                //TextSpan.......
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
